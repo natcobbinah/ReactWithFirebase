cdfd0762f08444d2ea18dda6ed5db012b10ee758
natcobbinah <baby.hugo74@yahoo.com>
2021-02-26 22:26:22 +0000

    firebase Authentication APIs instantiated

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,3 +1,6 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -7,3 +10,28 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   };
+
+  class Firebase{
+    constructor(){
+        app.initializeApp(firebaseConfig);
+        app.analytics();
+        this.auth = app.auth();
+    }
+
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email,password) => 
+        this.auth.createUserWithEmailAndPassword(email,password);
+
+    doSignInWithEmailAndPassword = (email,password) =>
+        this.auth.signInWithEmailAndPassword(email,password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => 
+        this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => 
+        this.auth.currentUser.updatePassword(password);
+  }
+
+  export default Firebase
