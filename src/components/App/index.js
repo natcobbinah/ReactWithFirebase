41bfaba3e33c17125ca8f5de00372e564d02d819
natcobbinah <baby.hugo74@yahoo.com>
2021-02-27 16:11:21 +0000

    session management implemented with reactContext

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{Component} from 'react'
 import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Navigation from '../Navigation'
 import Landing from '../Landing'
@@ -10,19 +10,46 @@ import Admin from '../Admin'
 import Account from '../Account'
 
 import * as ROUTES from '../constants/routes'
+import {withFirebase} from '../Firebase'
 
-const App = () => (
-    <Router>
-        <Navigation/>
-        <hr/>
-        <Route exact path={ROUTES.LANDING} component={Landing}/>
-        <Route  path={ROUTES.SIGN_UP} component={SignUp}/>
-        <Route  path={ROUTES.SIGN_IN} component={SignIn}/>
-        <Route  path={ROUTES.PASSWORD_FORGET} component={PasswordForget}/>
-        <Route  path={ROUTES.HOME} component={Home}/>
-        <Route  path={ROUTES.ACCOUNT} component={Account}/>
-        <Route  path={ROUTES.ADMIN} component={Admin}/>
-    </Router>
-)
+class App extends  Component {
+    constructor(props){
+        super(props);
 
-export default App
+        this.state = {
+            authUser: null,
+        }
+    }
+
+    componentDidMount(){
+        this.props.firebase.auth.onAuthStateChanged(authUser => {
+            authUser?
+                this.setState({authUser}) :
+                this.setState({authUser:null})
+        })
+    }
+
+    componentWillUnmount(){
+        this.listener();
+    }
+
+    render(){
+        return(
+        <Router>
+          <div>
+            <Navigation authUser={this.state.authUser}/>
+            <hr/>
+            <Route exact path={ROUTES.LANDING} component={Landing}/>
+            <Route  path={ROUTES.SIGN_UP} component={SignUp}/>
+            <Route  path={ROUTES.SIGN_IN} component={SignIn}/>
+            <Route  path={ROUTES.PASSWORD_FORGET} component={PasswordForget}/>
+            <Route  path={ROUTES.HOME} component={Home}/>
+            <Route  path={ROUTES.ACCOUNT} component={Account}/>
+            <Route  path={ROUTES.ADMIN} component={Admin}/>
+          </div>
+        </Router>
+        );
+    }
+}
+
+export default withFirebase(App)
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
 import * as ROUTES from '../constants/routes'
 
 import SignOut from '../SignOut'
 
-const Navigation = () => (
-    <div>
-       <ul>
-           <li>
-               <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-           </li>
+const  Navigation = ({authUser}) => (
+       <div>
+        {authUser ?
+                <NavigationAuth/>:
+                <NavigationNonAuth/>
+        }
+        </div>
+)
+
+const NavigationAuth = () => (
+    <ul>
            <li>
                <Link to={ROUTES.LANDING}>Landing</Link>
            </li>
@@ -20,13 +25,20 @@ const Navigation = () => (
                <Link to={ROUTES.ACCOUNT}>Account</Link>
            </li>
            <li>
-               <Link to={ROUTES.ADMIN}>Admin</Link>
-           </li>
-           <li>
                <SignOut/>
            </li>
        </ul>
-    </div>
+)
+
+const NavigationNonAuth = () => (
+    <ul>
+        <li>
+               <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+               <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+    </ul>
 )
 
 export default Navigation
